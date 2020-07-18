37760be61c56349539e1b8573f5b2e9cef219164
Alek <alekissac@gmail.com>
2020-07-18 14:09:29 -0500

    Login endpoint added

--- app/src/main/java/com/example/proyecto_semestral_checkpoint/network/Recipe_App_API.java
+++ app/src/main/java/com/example/proyecto_semestral_checkpoint/network/Recipe_App_API.java
@@ -1,16 +1,22 @@
 package com.example.proyecto_semestral_checkpoint.network;
 
+import com.example.proyecto_semestral_checkpoint.models.Log_In_User;
 import com.example.proyecto_semestral_checkpoint.models.Recipe;
+import com.example.proyecto_semestral_checkpoint.models.User;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface Recipe_App_API {
 
-@GET("recipes")
-Call<ArrayList<Recipe>> getRecipe(@Header("Authorization") String token);
+    @POST("users/login")
+    Call<Log_In_User> login(@Body User user);
+
+    @GET("recipes")
+    Call<ArrayList<Recipe>> getRecipe(@Header("Authorization") String token);
 }
