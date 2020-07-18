47f7bd4dc4eb0ff6d15895331b8754c91096cbc5
Alek <alekissac@gmail.com>
2020-07-17 23:25:13 -0500

    get recipes endpoint created

--- app/src/main/java/com/example/proyecto_semestral_checkpoint/network/Recipe_App_API.java
+++ app/src/main/java/com/example/proyecto_semestral_checkpoint/network/Recipe_App_API.java
@@ -1,4 +1,16 @@
 package com.example.proyecto_semestral_checkpoint.network;
 
+import com.example.proyecto_semestral_checkpoint.models.Recipe;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
 public interface Recipe_App_API {
+
+@GET("recipes")
+Call<ArrayList<Recipe>> getRecipe(@Header("Authorization") String token);
 }
