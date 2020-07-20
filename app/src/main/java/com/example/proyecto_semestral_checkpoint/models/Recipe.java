e92039d6581b92a19504edfeff0daa8db35c1d3c
Alek <alekissac@gmail.com>
2020-07-20 18:42:18 -0500

    Recipe class implements Parcelable

--- app/src/main/java/com/example/proyecto_semestral_checkpoint/models/Recipe.java
+++ app/src/main/java/com/example/proyecto_semestral_checkpoint/models/Recipe.java
@@ -1,9 +1,12 @@
 package com.example.proyecto_semestral_checkpoint.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Recipe {
+public class Recipe implements Parcelable {
 
     private String _id;
     private String name;
@@ -12,6 +15,35 @@ public class Recipe {
     private String author;
     private ArrayList<HashMap<String, String>> ingredients;
     private int upvotes;
+
+    public Recipe(Parcel in) {
+        _id = in.readString();
+        name = in.readString();
+        category = in.readInt();
+        description = in.readString();
+        author = in.readString();
+        upvotes = in.readInt();
+    }
+
+    public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel in) {
+            return new Recipe(in);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+
+    public Recipe(String name, int category, String description, String author, ArrayList<HashMap<String, String>> ingredients) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.author = author;
+        this.ingredients = ingredients;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -59,5 +91,20 @@ public class Recipe {
 
     public int getUpvotes() {
         return upvotes;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(_id);
+        dest.writeString(name);
+        dest.writeInt(category);
+        dest.writeString(description);
+        dest.writeString(author);
+        dest.writeInt(upvotes);
     }
 }
